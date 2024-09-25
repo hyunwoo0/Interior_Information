e19a2dc67f83ac9a5addd58216ce75e01acb88cb
hyunwoo0 <baghyeonu123@gmail.com>
2024-09-25 13:56:05 +0900

    [chore]

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+import Header from './components/header.jsx';
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+    <header>
+        <h1>디자인스튜디오</h1>
+    </header>
+    );
+}
+
+export default Header;
